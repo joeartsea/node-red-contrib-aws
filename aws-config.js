1f8dc0fb2e6720f2ef9dcb77182effa82d8f0353
daniel <daniel.thomas@gmail.com>
2016-09-24 12:20:53 +1000

    Fixes #5 AWS credentials not working

--- aws-config.js
+++ aws-config.js
@@ -1,10 +1,11 @@
 module.exports = function(RED) {
     function RemoteServerNode(n) {
         RED.nodes.createNode(this,n);
-        this.accessKey = n.accessKey;
-        this.secretKey = n.secretKey;
+        this.accessKey = this.credentials.accessKey;
+        this.secretKey = this.credentials.secretKey;
 	      this.region = n.region;
 	      this.name = n.name;
+//        console.log(this.credentials);
     }
     RED.nodes.registerType("aws-config",RemoteServerNode,{credentials: {
          accessKey: {type:"text"},
